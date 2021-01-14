14e6b05893c7d4b06bd325689d62416b89876cb9
AllanPessin <allan.pessin@outlook.com>
2021-01-13 21:59:50 -0300

    Mobile Finish

--- front-mobile/src/OrderCard/index.tsx
+++ front-mobile/src/OrderCard/index.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View  } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Order } from '../types';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 
+dayjs.locale('pt-br');
+dayjs.extend(relativeTime);
 
+type Props = {
+  order: Order
+}
 
-function OrderCard() {
+function dateFromNow(date: string) {
+  return dayjs(date).fromNow();
+}
+
+function formatPrice(price: number) {
+  const formmater = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+      minimumFractionDigits: 2
+  });
+  return formmater.format(price)
+}
+
+function OrderCard({order}: Props) {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.orderName}>Pedido 1</Text>
-        <Text style={styles.orderPrice}>R$ 50,00</Text>
+        <Text style={styles.orderName}>{order.id}</Text>
+        <Text style={styles.orderPrice}>{formatPrice(order.total)}</Text>
       </View>
-      <Text style={styles.text}>Há 30min</Text>
+      <Text style={styles.text}>{dateFromNow(order.moment)}</Text>
       <View style={styles.productsList}>
-        <Text style={styles.text}>Pizza Calabresa</Text>
-        <Text style={styles.text}>Pizza Calabresa</Text>
-        <Text style={styles.text}>Pizza Macarrão</Text>
-        <Text style={styles.text}>Pizza Queijo</Text>
+        {order.products.map(product => (
+          <Text key={product.id} style={styles.text}>{product.name}</Text>
+        ))}
       </View>
     </View>
   );
--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,17 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView  } from 'react-native';
+import { useNavigation, useIsFocused } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, Alert  } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { fetchOrders } from '../api';
 import Header from '../Header';
 import OrderCard from '../OrderCard';
+import { Order } from '../types';
 
 
 function Orders() {
+  const [orders, setOrders] = useState <Order[]>([]);
+  const [isLoading, setIsLoading] = useState (false);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  const fetchData = () => {
+    setIsLoading(true);
+    fetchOrders()
+    .then(response => setOrders(response.data))
+    .catch( () => Alert.alert("Houve um erro ao buscar os pedidos"))
+    .finally(() => setIsLoading(false));
+  }
+
+  useEffect (() => {
+    if (isFocused) {
+      fetchData();
+    }
+  }, [isFocused]);
+
+  
+    const handlOnPress = (order: Order) => {
+        navigation.navigate('OrderDetails', {
+          order
+        });
+    }
+
   return (
     <>
       <Header/>
       <ScrollView style={styles.container}>
-        <OrderCard/>
-        <OrderCard/>
-        <OrderCard/>
+        {isLoading? (
+          <Text>Buscando pedidos...</Text>
+        ) : (orders.map(order => (
+          <TouchableWithoutFeedback key={order.id} onPress={() => handlOnPress(order)}>
+            <OrderCard order={order} />
+          </TouchableWithoutFeedback>
+        ))) }
       </ScrollView>
     </>
   );
